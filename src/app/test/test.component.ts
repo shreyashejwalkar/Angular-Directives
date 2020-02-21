c0af5368f1ad786a8b064983424acf8696639822
shreyashejwalkar <shreyashejwalkar1610@gmail.com>
2020-02-21 12:22:36 +0530

    ngSwitch Implemented

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -14,11 +14,19 @@ import { Component, OnInit } from '@angular/core';
   <div *ngIf="displayname; then thenBlock; else elseBlock"></div>
   <ng-template #thenBlock><h2>From then block</h2></ng-template>
   <ng-template #elseBlock><h2>From else block</h2></ng-template>
+
+  <!--ngSwitch -->
+  <div [ngSwitch]="color">
+    <div *ngSwitchCase="'red'"><h2 style="color:red;">You Picked Red color</h2></div>
+    <div *ngSwitchCase="'blue'"><h2 style="color:blue;">You Picked Blue color</h2></div>
+    <div *ngSwitchDefault><h2>Pick again</h2></div>
+  </div>
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit 
 {
+  public color = "blue";
   displayname = true;
   constructor() { }
   ngOnInit(): void {}
