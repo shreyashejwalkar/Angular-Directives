e0312a3484b937b217e778561bdce7f8061629b3
shreyashejwalkar <shreyashejwalkar1610@gmail.com>
2020-02-21 12:12:17 +0530

    ngIf Implemented

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+
+  <!--ngIf first way -->
+  <h2 *ngIf="displayname; else elseBlock">Structural Directives</h2>
+  <ng-template #elseBlock>
+    <h2>Name is hidden</h2>
+  </ng-template>
+
+  <!--ngIf Second way -->
+  <div *ngIf="displayname; then thenBlock; else elseBlock"></div>
+  <ng-template #thenBlock><h2>From then block</h2></ng-template>
+  <ng-template #elseBlock><h2>From else block</h2></ng-template>
+  `,
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
-
+export class TestComponent implements OnInit 
+{
+  displayname = true;
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
